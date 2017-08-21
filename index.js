20ad3f2f5076960f03505b2822d7a22823bdca79
Mzizio <mtshalimxolisii@gmail.com>
2017-08-21 12:26:25 +0200

    changed port number

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ app.get('/counter/:names', function(req, res) {
 //     console.log('node server.js', host, port);
 // });
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 3000;
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen('port', function() {
+  console.log('Node app is running on port', + port);
 });
